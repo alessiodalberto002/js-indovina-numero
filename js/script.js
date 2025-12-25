e3fe3a7828e0893d313eed7440faf80ac4f08d35
alessiodalberto002 <aledalberto002.com>
2025-12-25 23:56:37 +0100

    Ho scritto una funzione che generi un numero casuale da 1 a 100

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 // VARIABILI GLOBALI //
 let numeroDaIndovinare;
 let conteggioTentativi = 0;
+
+// GENERIAMO UN NUMERO CASUALE DA 1 A 100 //
+function generaNumero() {
+    return Math.floor(Math.random() * 100) + 1;
+}
