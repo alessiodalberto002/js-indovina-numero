5d561c865f19ec793d468eeb4c27d37024ec51e7
alessiodalberto002 <aledalberto002.com>
2025-12-25 23:57:55 +0100

    Ho scritto una funzione che inizializza il gioco all'avvio

--- js/script.js
+++ js/script.js
@@ -6,3 +6,14 @@ let conteggioTentativi = 0;
 function generaNumero() {
     return Math.floor(Math.random() * 100) + 1;
 }
+
+// INIZIALIZZIAMO IL GIOCO ALL'AVVIO //
+function inizializzaGioco() {
+    numeroDaIndovinare = generaNumero();
+    conteggioTentativi = 0;
+    document.getElementById('messaggio').textContent = '';
+    document.getElementById('tentativi').textContent = 'Tentativi: 0';
+    document.getElementById('tentativo').value = '';
+    document.getElementById('tentativo').disabled = false;
+    console.log('Numero segreto:', numeroDaIndovinare); // Per test
+}
